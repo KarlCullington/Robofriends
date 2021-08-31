84357000f34d921c36ad11334fd98dae49bd8115
KPC-Web <75866826+KPC-Web@users.noreply.github.com>
2021-08-31 08:21:40 +0200

    Robofriends Commit

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,15 +1,22 @@
 import React from 'react';
 
-const Card = ({ name, email, id }) => {
-  return (
-    <div className='tc grow bg-light-green br3 pa3 ma2 dib bw2 shadow-5'>
-      <img alt='robots' src={`https://robohash.org/${id}?size=200x200`} />
-      <div>
-        <h2>{name}</h2>
-        <p>{email}</p>
-      </div>
-    </div>
-  );
-}
+const Card = ({ name, username, email, id }) => {
+    return (
+        <div className='tc grow bg-light-green br3 pa3 ma2 dib bw2 shadow-5'>
+            <img alt='robots' src={`https://robohash.org/${id}?size=200x200`} />
+            <div>
+                <h2>{name}</h2>
+                <p>
+                    <span className='b'>Username: </span>
+                    {username}
+                </p>
+                <p>
+                    <span className='b'>Email: </span>
+                    {email}
+                </p>
+            </div>
+        </div>
+    );
+};
 
 export default Card;
